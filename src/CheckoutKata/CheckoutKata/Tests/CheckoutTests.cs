4bc66f683089b30045e23ae0bd4f82f946358661
Rob Bentley <rob@robbentley.net>
2020-05-18 19:20:22 +0100

    Setup the Project

---
+++ src/CheckoutKata/CheckoutKata/Tests/CheckoutTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace CheckoutKata.Tests
+{
+    public class CheckoutTests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
